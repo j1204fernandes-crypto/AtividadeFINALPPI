f23ab10372fa35c34230338c6de3705e895cc700
João Gabriel <j1204fernandes@gmail.com>
2025-12-08 19:01:59 -0300

    add a func do botao add equipes

--- index.js
+++ index.js
@@ -75,6 +75,174 @@ server.get("/", verificarUsuarioLogado, (requisicao, resposta) => {
     resposta.end();
 });
 
+server.get("/cadastroEquipes", verificarUsuarioLogado,(requisicao,resposta) => {
+    resposta.send(`
+        <DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                <title>Menu do Sistema</title>
+            </head>
+            <body style="background-color: rgb(137, 137, 137);">
+              <div class="d-flex justify-content-center align-items-center vh-100 ">
+                <div class="container">
+                    <h1 class="text-center border m-3 p-3 bg-light">Cadastro de Equipes</h1>
+                    <form method="POST" action="/adicionarUsuario" class="row g-3 m-3 p-3 bg-light">
+                            <div class="col-md-4">
+                                <label for="nome" class="form-label">Nome da equipe</label>
+                                <input type="text" class="form-control" id="nome" name="nome">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="sobrenome" class="form-label">Nome do capitão</label>
+                                <input type="text" class="form-control" id="capitao" name="capitao">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="usuario" class="form-label">Telefone/whatsapp de contato</label>
+                                <input type="text" class="form-control" id="tel" name="tel" aria-describedby="inputGroupPrepend">
+                                </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary" type="submit">Cadastrar</button>
+                                <a class="btn btn-secondary" href="/">Voltar</a>
+                            </div>
+                    </form> <br><br><br><br><br><br><br><br>
+                </div>
+              </div>
+            </body>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        </html>
+    `);
+})
+
+server.post('/adicionarEquipes', verificarUsuarioLogado, (requisicao, resposta) => {
+    const nome = requisicao.body.nome;
+    const capitao = requisicao.body.capitao;
+    const tel = requisicao.body.tel;
+
+    if (nome && capitao && tel){
+
+        listaEquipes.push({nome, capitao, tel});
+        resposta.redirect("/listarEquipes");
+    }
+    else{
+
+        let conteudo = `
+        
+        <DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                <title>Menu do Sistema</title>
+            </head>
+            <body style="background-color: rgb(137, 137, 137);">
+              <div class="d-flex justify-content-center align-items-center vh-100 ">
+                <div class="container">
+                    <h1 class="text-center border m-3 p-3 bg-light">Cadastro de Equipes</h1>
+                    <form method="POST" action="/adicionarUsuario" class="row g-3 m-3 p-3 bg-light">
+                            <div class="col-md-4">
+                                <label for="nome" class="form-label">Nome da equipe</label>
+                                <input type="text" class="form-control" id="nome" name="nome" value="${nome}">
+                            `;
+        if (!nome){
+            conteudo += `
+            <div>
+                <p class="text-danger">Por favor, informe o nome da equipe</p>
+            </div>
+        `
+        }
+
+        conteudo += `</div>
+                            <div class="col-md-4">
+                                <label for="sobrenome" class="form-label">Nome do capitão</label>
+                                <input type="text" class="form-control" id="capitao" name="capitao" value="${capitao}">
+                            `;
+        if (!capitao){
+        conteudo += `
+            <div>
+                <p class="text-danger">Por favor, informe o Nome do capitão</p>
+            </div>
+        `;
+        }
+
+        conteudo+= `</div>
+                            <div class="col-md-4">
+                                <label for="usuario" class="form-label">Telefone/whatsapp de contato</label>
+                                <input type="text" class="form-control" id="tel" name="tel" aria-describedby="inputGroupPrepend" value="${tel}">
+                                
+                            `;
+        if (!tel){
+
+            conteudo += `
+                <div>
+                    <p class="text-danger">Por favor, informe o Telefone de contato</p>
+                </div>
+            `;
+        }
+
+        conteudo += `</div>
+                            <div class="col-12">
+                                <button class="btn btn-primary" type="submit">Cadastrar</button>
+                                <a class="btn btn-secondary" href="/">Voltar</a>
+                            </div>
+                    </form> <br><br><br><br><br><br><br><br>
+                </div>
+                </div>
+            </body>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        </html>
+        
+        `;
+
+        resposta.send(conteudo);
+
+    }
+
+    });
+
+server.get("/listarEquipes", verificarUsuarioLogado,(requisicao, resposta) => {
+    let conteudo = `
+       <DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                <title>Lista de usuários no Sistema</title>
+            </head>
+            <body style="background-color: rgb(137, 137, 137);">
+                 <div class="container">
+                  <div class="border m-3 p-3 bg-light">
+                      <h1 class="text-center border m-3 p-3 bg-light">Lista de Equipes</h1>
+                      <table class="table table-striped table-hover">
+                          <thead>
+                              <tr>
+                                  <th>Nome da Equipe</th>
+                                  <th>Nome do Capitão</th>
+                                  <th>Telefone/whatsapp de contato</th>
+                              </tr>
+                          </thead>
+                          <tbody>`;
+    for (let i = 0; i < listaEquipes.length; i++) {
+        conteudo += `
+            <tr>
+                <td>${listaEquipes[i].nome}</td>
+                <td>${listaEquipes[i].capitao}</td>
+                <td>${listaEquipes[i].tel}</td
+            </tr>
+        `;
+    }
+    conteudo+=`
+                         </tbody>
+                      </table>
+                      <a class="btn btn-secondary" href="/cadastroUsuario">Voltar</a>
+                  </div>
+                </div> 
+            </body>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        </html>
+    `
+    resposta.send(conteudo);
+});
 
 
 
